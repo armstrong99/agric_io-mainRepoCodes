d609079d31ac596929d7b77fb3330b84fe15d254
armstrong99 <armstrong.ndukwe@gmail.com>
2020-11-20 14:14:29 +0100

    addded vue material

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import './assets/styles/tailwind.css'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(store).use(router).use(MdButton).use(MdContent).use(MdTabs).mount('#app')
