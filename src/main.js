88e7c0ccc3785d4611a01440816b496676820e8f
armstrong99 <armstrong.ndukwe@gmail.com>
2020-11-20 12:47:20 +0100

    error in bootstrap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// import '@babel/polyfill'
+// import 'mutationobserver-shim'
+// import './plugins/bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles/tailwind.css'
@@ -5,7 +8,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App).use(store).use(BootstrapVue).use(BootstrapVueIcons).use(router).mount('#app')
